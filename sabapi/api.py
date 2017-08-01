d84ad5cb79356064657e2a24af105aad83113b3e
Hellowlol <hellowlol1@gmail.com>
2017-08-01 23:53:30 +0200

    clnup

--- sabapi/api.py
+++ sabapi/api.py
@@ -299,31 +299,3 @@ class Sabnzbd(object):
     @asyncio.coroutine
     def warnings(self):
         return (yield from self._query('warnings'))
-
-
-
-
-
-
-
-
-
-
-
-"""
-from collections import defaultdict
-
-class AttributeDict(defaultdict):
-    def __init__(self):
-        super(AttributeDict, self).__init__(AttributeDict)
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(key)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-"""
