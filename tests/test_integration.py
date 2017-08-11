cef84d2344c9f7128f2637eb60d1c3c04bfe06df
Hellowlol <hellowlol1@gmail.com>
2017-08-11 23:13:28 +0200

    fixup

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,13 +2,13 @@ import asyncio
 
 loop = asyncio.get_event_loop()
 
-def test_auth(sab):
+def _test_auth(sab):
     t = loop.run_until_complete(sab.auth())
 
-def test_warnings(sab):
+def _test_warnings(sab):
     t = loop.run_until_complete(sab.warnings())
     assert 'warnings' in t
 
-def test_server_stats(sab):
+def _test_server_stats(sab):
     t = loop.run_until_complete(sab.server_stats())
     assert 'week' in t
